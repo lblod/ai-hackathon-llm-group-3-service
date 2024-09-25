a9ec562b79ac16076564e5f73acc1cd7fc35ef6c
Louis BEZ <louis.bez@inetum.com>
2024-09-25 14:24:02 +0200

    upgraded hf_local_llm class

--- scripts/hf_local_llm.py
+++ scripts/hf_local_llm.py
@@ -2,11 +2,31 @@ import torch
 from transformers import pipeline
 
 class LocalHFLLM:
-    def __init__(self, model_name="BramVanroy/fietje-2-instruct"):
+    def __init__(self, model_name="BramVanroy/fietje-2-instruct",
+                 max_new_tokens=256,
+                 do_sample=False,
+                 temperature=0.7,
+                 top_k=50,
+                 top_p=0.95):
         self.model = pipeline("text-generation",
                               model=model_name,
                               torch_dtype=torch.bfloat16,
                               device_map="auto")
+        self.max_new_tokens = max_new_tokens
+
+    def run(self, messages):
+        prompt = model.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        if self.do_sample:
+            outputs = model(prompt,
+                            max_new_tokens=self.max_new_tokens,
+                            do_sample=True,
+                            temperature=self.temperature,
+                            top_k=self.top_k,
+                            top_p=self.top_p)
+        else:
+            outputs = self.model(prompt, max_new_tokens=self.max_new_tokens, do_sample=False)
+        full_answer = outputs[0]["generated_text"]
+        return full_answer
 
     def request(system_prompt, user_prompt):
         messages = [
@@ -19,10 +39,7 @@ class LocalHFLLM:
                 "content": user_prompt
             }
         ]
-        prompt = model.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        # outputs = model(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-        outputs = self.model(prompt, max_new_tokens=256, do_sample=False)
-        full_answer = outputs[0]["generated_text"]
+        full_answer = self.run(messages)
         answer = full_answer.split('<|assistant|>')[1]
         return answer
 
@@ -37,9 +54,6 @@ class LocalHFLLM:
                 "content": f"{user_prompt}\ncontext:{context}"
             }
         ]
-        prompt = model.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        # outputs = model(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-        outputs = self.model(prompt, max_new_tokens=256, do_sample=False)
-        full_answer = outputs[0]["generated_text"]
+        full_answer = self.run(messages)
         answer = full_answer.split('<|assistant|>')[1]
         return answer
